75a33833a438a2bc524f77016778c1d1944de3fa
Nhut Huynh <Ne7ven@yahoo.com>
2013-12-04 00:36:52 +0700

    first create aDUng

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿$(document).bind('ready', function () {
+    $('[data-ride="carousel"]').each(function () {
+        var $carousel = $(this);
+        console.log($carousel.data());
+        $carousel.carousel($carousel.data());
+    });
+
+});
